f8b60cefc8e022bd6d7da143380bfb83f6331cc1
Howard Wilson <howard@watsonbox.net>
2014-10-13 18:41:30 +0200

    Adapt formulae for head-only versions

--- cmu-pocketsphinx.rb
+++ cmu-pocketsphinx.rb
@@ -1,14 +1,18 @@
-require 'formula'
+require "formula"
 
 class CmuPocketsphinx < Formula
   homepage 'http://cmusphinx.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/cmusphinx/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz'
-  sha1 'd9efdd0baddd2e47c2ba559caaca62ffa0c0eede'
+  head "https://github.com/cmusphinx/pocketsphinx.git"
 
   depends_on 'pkg-config' => :build
-  depends_on 'cmu-sphinxbase'
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+
+  depends_on 'watsonbox/cmu-sphinx/cmu-sphinxbase'
 
   def install
+    system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
--- cmu-sphinxbase.rb
+++ cmu-sphinxbase.rb
@@ -2,16 +2,21 @@ require "formula"
 
 class CmuSphinxbase < Formula
   homepage "http://cmusphinx.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/cmusphinx/sphinxbase/0.8/sphinxbase-0.8.tar.gz"
-  sha1 "c0c4d52e143d07cd593bd6bcaeb92b9a8a5a8c8e"
+  head "https://github.com/cmusphinx/sphinxbase.git"
 
   depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   # If these are found, they will be linked against and there is no configure
   # switch to turn them off.
   depends_on "libsndfile"
   depends_on "libsamplerate" => "with-libsndfile"
+  depends_on "swig"
 
   def install
+    system "./autogen.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
